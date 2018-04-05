726e63589319eec0c2a12fe3842a3d0e8ab13105
Shantanu Chhabra <shantanu_chhabra@apple.com>
2018-04-05 15:18:40 -0700

    the experiment continues

--- src/visualization/Turi Create Visualization/src/js/webpack.config.vg2png.js
+++ src/visualization/Turi Create Visualization/src/js/webpack.config.vg2png.js
@@ -5,17 +5,11 @@ module.exports = {
   entry: './node_modules/vega/bin/vg2png',
   target: 'node',
   module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                // test: [/.js$/, 'vg2png'],
-                // include : [APP_DIR, './node_modules/vega/bin/'],
-                options: {
-                    presets: [ 'es2015', 'react' ]
-                }
-            }
-        ]
-    },
+    rules: [{
+      test: /\.node$/,
+      use: 'node-loader'
+    }]
+  },
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'vg2png.js'
